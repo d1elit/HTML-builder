505102fed1bf0cf588aa6987163af8390feb26f5
Artem Gromiko <dropdox12@gmail.com>
2022-11-07 01:52:47 +0300

    feat: add solution for 04-copy-derictory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path')
+const fs = require('fs')
 
+const copyDir = (src,dist) => {
+  fs.rm(path.join(__dirname,'files-copy'), { recursive: true, force: true }, err => {
+    fs.mkdir(path.join(__dirname,'files-copy'), { recursive: true }, err => {
+      if (err) throw err;
+      fs.readdir( path.join(__dirname, 'files'), (err,files) => {
+        if (err) throw err;
+        files.forEach(file => {      
+          fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy',file), err => {if (err) throw err})        
+        })
+      })      
+    })
+  })
+
+
+  // 
+}
+
+copyDir()
