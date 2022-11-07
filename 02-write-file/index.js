d413df7b45347addab93ded7361186a6d3f90fb4
Artem Gromiko <dropdox12@gmail.com>
2022-11-08 00:23:42 +0300

    fix: delete exit word from text file after input end in 02-wire-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -11,22 +11,22 @@ process.stdout.write('Введите строку'+'\n')
 writeData(false)
 
 function writeData(isEnd) { 
+  if(isEnd) {
+    process.stdout.write('До встречи'+'\n')
+    process.exit()
+  } 
   fs.writeFile(path.join(__dirname,'text.txt'), results.join(' '), err => {
-    if(err) throw err;   
-    if(isEnd) {
-      process.stdout.write('До встречи'+'\n')
-      process.exit()
-    } 
+    if(err) throw err;      
   })  
 }
 
 stdin.on('data', function(data) {
-  let strData = data.toString('utf8').trim()   
-  results.push(strData)
-  writeData(false)
+  let strData = data.toString('utf8').trim()  
   if(strData === 'exit') { 
     writeData(true)
     }   
+   results.push(strData)
+   writeData(false)
 
 })
 
